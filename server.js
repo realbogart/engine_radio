b2019384256206ece4663b701d8c59b4c6b3d58d
realbogart <johan.yngman@gmail.com>
2016-12-27 15:26:35 +0100

    Piping stream through nodejs server and automatic reload of css file

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(req, res){
-    fs.readFile('index.html',function (err, data){
-        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-        res.write(data);
-        res.end();
-    });
-}).listen(1337);
+var server = http.createServer();
+
+server.on('request', function(request, response){
+	var method = request.method;
+	var url = request.url;
+	var headers = request.headers;
+
+	if(url == "/swyh") {
+		var request = require('request');
+		request('http://127.0.0.1:1485/stream/swyh.mp3').pipe(response);
+	}
+	else if(url == "/style"){
+		fs.readFile('style.css', function (err, data){
+        	response.writeHead(200, {'Content-Type': 'text/css','Content-Length':data.length});
+       		response.write(data);
+        	response.end();
+    	});
+	}
+	else{
+		fs.readFile('index.html', function (err, data){
+        	response.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+       		response.write(data);
+        	response.end();
+    	});
+	}
+});
+
+server.listen(1337);
