2c0b42532b557a89e7764778f74ec1bf9aeb4a05
Johan Yngman <johan.yngman@gmail.com>
2023-10-09 16:11:40 +0200

    Admin login wip

---
+++ admin_login.js
@@ -0,0 +1,33 @@
+document.getElementById('passwordForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    var password = document.getElementById('password').value;
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            password: password
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            if(response.status === 401) {
+                alert("Invalid password");
+            } else {
+                alert("Server error");
+            }
+            throw new Error('Request failed: ' + response.status);
+        }
+        return response.text();
+    })
+    .then(html => {
+      //document.documentElement.innerHTML = html;
+    })
+    .catch(error => {
+        console.error("Error: ", error);
+    });
+});
+
